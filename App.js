5032f6e50eadfd3feb833a619ac9dc1a9b3cd68c
sandeep <abhisandeep@1998>
2024-01-01 13:02:49 +0530

    commit pro

--- App.js
+++ App.js
@@ -1,121 +1,77 @@
-const newsContainer = document.querySelector("#newsContainer");
-const saveButton = document.querySelector("#saveButton");
-const loadSavedButton = document.querySelector("#loadSavedButton");
-const loadNewsButton = document.querySelector("#loadNewsButton");
-const categorySelect = document.querySelector("#categorySelect");
 
-function showDiv() {
-  document.querySelector('#hidden').style.display = "block";
+
+
+const API_KEY = "3d8e106189564c6cab2c19e447327a86"
+const url = "https://newsapi.org/v2/everything?q="
+
+
+
+async function fetchData(query){
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`)
+    const data = await res.json()
+    return data
 }
+fetchData("all").then(data => renderMain(data.articles))
 
-const savedNews = [];
-const handleSavedNews = (savedItem) => {
-  savedNews.push(savedItem);
-  console.log(savedNews);
-  alert("News saved")
-  saveNews(savedItem);
 
-}
+let mobilemenu = document.querySelector(".mobile")
+let menuBtn = document.querySelector(".menuBtn")
+let menuBtnDisplay = true;
 
-const getNews = (category = "science") => {  
-   const Key = "Kk072FuHb36o2CXysAJ6zfyZ1GKIF_RJdtLPnDUizNZ8I968"
-  newsContainer.innerHTML = "";
-  fetch(`https://api.currentsapi.services/v1/latest-news?apiKey=${Key}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Data", data);
-      data.news.forEach((newsItem) => {
-        const div = document.createElement("div");
-        div.classList.add("newsItem");
-        div.innerHTML = `
-          <p>By - <strong>${newsItem.author}</strong></p>
-          <h2>${newsItem.title}</h2>
-          <div id="box">
-          <img src="${newsItem.image}" class="img"></img>
-          <div id="innerbox">
-          <p id="nscontent">${newsItem.description} <a href="${newsItem.url}" style="text-decoration:none">READ MORE</a></p>
-          <p>Date:- ${newsItem.published}</p>
-          </div>
-          </div>
-        `;
-        const button = document.createElement("button");
-        button.classList.add("btton")
-        button.innerHTML = "SAVE"
-        button.onclick = function () {
-          handleSavedNews(newsItem)
+menuBtn.addEventListener("click",()=>{
+    mobilemenu.classList.toggle("hidden")
+})
 
-        }
-        const i = document.createElement("i");
-        i.classList.add("fa-solid")
-        i.classList.add("fa-heart")
-        i.onclick = function () {
-          handlelikedNews(newsItem)
-        }
-        i.addEventListener("click", () => {
-          i.classList.toggle("red");
-    
-        })
 
-        div.appendChild(button);
-        div.appendChild(i);
-        newsContainer.appendChild(div);
-      });
-    });
-    categorySelect.addEventListener("change", function() {
-      slectedCategory = categorySelect.value;
-      fetch(`https://api.currentsapi.services/v1/latest-news?category=${slectedCategory}&apiKey=${Key}`)
-        .then((response) => response.json())
-       .then((json) => {
-         console.log(json)
-          // renderCategoryNews(json);
-        });
-      });
-};
-
-const saveNews = (id) => {
-  const newss = Array.from(document.querySelectorAll(".newsItem")).map(
-    (newsItem) => {
-      return {
-        title: newsItem.querySelector("h2").textContent,
-        content: newsItem.querySelector("#nscontent").textContent,
-      };
-    }
-  );
-  console.log("saved news",id)
-  localStorage.setItem("savedNews", JSON.stringify(id));
-  JSON.parse(localStorage.getItem("newss"));
-};
-
-const likeNews = JSON.parse(localStorage.getItem("likeNews")) || [];
-const handlelikedNews = (likeItem) => {
-  likeNews.push(likeItem);
-  localStorage.setItem("likeNews", JSON.stringify(likeNews))
-  console.log(likeNews);
  
+function renderMain(arr){
+    let mainHTML = ''
+    for(let i = 0 ; i < arr?.length;i++){
+        if(arr[i].urlToImage){
+        mainHTML += ` <div class="card">
+                        <a href=${arr[i]?.url}>
+                        <img src=${arr[i]?.urlToImage} lazy="loading" alt="" />
+                        <h4>${arr[i]?.title}</h4>
+                        <div class="publishbyDate">
+                            <p>${arr[i]?.source.name}</p>
+                            <span>•</span>
+                            <p>${new Date(arr[i]?.publishedAt).toLocaleDateString()}</p>
+                        </div>
+                        <div class="desc">
+                           ${arr[i]?.description}
+                        </div>
+                        </a>
+                     </div>
+        `
+        }
+    }
+
+    document.querySelector("main").innerHTML = mainHTML
 }
 
-const loadSavedNews = () => {
-  console.log("Saved News", savedNews)
-  newsContainer.innerHTML = "";
-  
-  if (!savedNews) {
-    return;
-  }
-  savedNews.forEach((newsItem) => {
-    const div = document.createElement("div");
-    div.classList.add("newsItem");
-    div.innerHTML = `
-    <h2>${newsItem.title}</h2>
-    <p>${newsItem.content}</p>
-    
-    `;
-    newsContainer.appendChild(div);
-  });
-};
 
-loadSavedButton.addEventListener("click", loadSavedNews);
-loadNewsButton.addEventListener("click", () => {
-  getNews(categorySelect.value);
-});
+const searchBtn = document.getElementById("searchForm")
+const searchBtnMobile = document.getElementById("searchFormMobile")
+const searchInputMobile = document.getElementById("searchInputMobile") 
+const searchInput = document.getElementById("searchInput")
 
-getNews();
+
+searchBtn.addEventListener("click",async(e)=>{
+    e.preventDefault()
+   
+    const data = await fetchData(searchInput?.value)
+    renderMain(data?.articles)
+
+
+})
+searchBtnMobile.addEventListener("click",async(e)=>{
+    e.preventDefault()
+    const data = await fetchData(searchInputMobile?.value)
+    renderMain(data?.articles)
+})
+
+
+async function Search(query){
+    const data = await fetchData(query)
+    renderMain(data?.articles)
+}
